1437a6c20dc62ff0b78dd3331788bccd4fdc1959
malet <malet@invalid>
2019-06-22 23:01:06 +0200

    Configure basic fish

---
+++ config.fish
@@ -0,0 +1,26 @@
+alias o "rifle"
+alias ipy "python -c 'import IPython; IPython.terminal.ipapp.launch_new_instance()'"
+alias emacs "emacsclient -nc"
+alias mutt "neomutt"
+
+set -x TERMINAL "alacritty"
+set -x TERM "xterm-256color"
+set -x BROWSER "firefox"
+set -x EDITOR "nvim"
+set -x USE_EDITOR $EDITOR
+set -x VISUAL $EDITOR
+set -x DOWNLOADS "$HOME/Downloads"
+
+# Java Tiling fix
+export _JAVA_AWT_WM_NONREPARENTING=1
+
+# Util options
+set -x GREP_OPTIONS '--color=auto'
+set -x LESS '--ignore-case --raw-control-chars'
+set -x PAGER 'less'
+
+# Colors
+if status --is-interactive
+    set BASE16_SHELL "$HOME/.config/base16-shell/"
+    source "$BASE16_SHELL/profile_helper.fish"
+end
